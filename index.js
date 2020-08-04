49b19d706d7442bdfa7afc781e4acfe74235d7ba
dsun <dsun@hanmail.net>
2020-08-04 14:50:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love you Test02!');
+    res.send('I love you Test04!');
 });
 
 app.get('/start', function(req, res){
